ba9295f9c886342cce8646f333adaceb3b757c65
Shargeel Hayat <59932594+ShargHayat518@users.noreply.github.com>
2023-02-21 16:00:01 -0700

    - minor changes

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -1,1 +1,10 @@
 import React from 'react';
+import bg_image from "../bg_image.jpg";
+
+function HomeView() {
+    return (
+        <div class="bg-auto bg-no-repeat bg-center" style={{ backgroundImage: `url(${bg_image})` }}></div>
+    );
+}
+
+export default HomeView;
