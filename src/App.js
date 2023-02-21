b15db36f936b17fc37cc05af6a36e811bd1a73d3
myusername <jkelln24@gmail.com>
2023-02-21 15:56:06 -0700

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import HomeView from './components/HomeView';
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const url = "https://www.randyconnolly.com/funwebdev/3rd/api/movie/movies-brief.php?limit=10";
+        const response = await fetch(url);
+        const data = await response.json();
+        setMovies(data);
+      }
+      catch (err) {
+        console.error(err);
+      }
+    };
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <HomeView />;
+    </main>
   );
 }
 
