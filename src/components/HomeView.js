82a98b8ac46812880d417e4ba0b08362c753d2a2
Shargeel Hayat <59932594+ShargHayat518@users.noreply.github.com>
2023-02-22 16:56:38 -0700

    Improving search bar

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -13,23 +13,19 @@ const HomeView = props => {
                 <p class="text-2xl text-center font-semibold pb-16">Movie Browser</p>
                 <form class="grid grid-cols-10 pb-16">
                     <p class="text-2xl font-semibold">Title</p>
-                    <input class="col-start-3 col-end-9"></input>
+                    <input class="col-start-3 col-end-9 searchBar-area"></input>
                 </form>
                 <div class="grid grid-cols-10">
-                    <button class="col-start-3">Show Matching Movies</button>
-                    <button class="col-start-6">Show All Movies</button>
+                    <button class="col-start-3" onClick={handleViewClick}>Show Matching Movies</button>
+                    <button class="col-start-6" onClick={handleViewClick}>Show All Movies</button>
                 </div>
 
             </div>
         </div>
-    )
-        < div class="bg-auto bg-no-repeat bg-center" style = {{ backgroundImage: `url(${bg_image})` }
-}>
-            <h3>Movie Browser</h3>
-            <div className="searchBar-area">Title: <input type="text"></input></div>
-            <button onClick={handleViewClick}>Show Matching Movies</button><button onClick={handleViewClick}>Show All Movies</button>
-        </div >
     );
+
+
+    <div className="searchBar-area">Title: <input type="text"></input></div>
 }
 
 export default HomeView
