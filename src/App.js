4215abd61a47c218ec503e1c2b29f04a67ebfa8c
myusername <jkelln24@gmail.com>
2023-02-22 16:03:14 -0700

    Added search bar and buttons

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <main>
-      <HomeView />;
+      <HomeView movies={movies} />;
     </main>
   );
 }
--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import bg_image from "../bg_image.jpg";
 
-function HomeView() {
+const HomeView = props => {
+
+    const handleViewClick = () => {
+
+    }
+
     return (
-        <div class="bg-auto bg-no-repeat bg-center" style={{ backgroundImage: `url(${bg_image})` }}></div>
+        <div class="bg-auto bg-no-repeat bg-center" style={{ backgroundImage: `url(${bg_image})` }}>
+            <h3>Movie Browser</h3>
+            <div className="searchBar-area">Title: <input type="text"></input></div>
+            <button onClick={handleViewClick}>Show Matching Movies</button><button onClick={handleViewClick}>Show All Movies</button>
+        </div>
     );
 }
 
