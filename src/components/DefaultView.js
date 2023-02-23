c6d2c459392347924bc058e7258650d0d37fd0a5
Shargeel Hayat <59932594+ShargHayat518@users.noreply.github.com>
2023-02-23 15:51:36 -0700

    improvements to movie filters area

--- src/components/DefaultView.js
+++ src/components/DefaultView.js
@@ -3,7 +3,7 @@ import Header from './Header'
 
 const DefaultView = props => {
     return (
-        <div class="grid grid-cols-8 grid-rows-10 gap-10">
+        <div class="grid grid-cols-8 grid-rows-10 gap-10 font-sans text-black">
 
             {/*LOGO*/}
             <div class="col-span-8">
@@ -11,18 +11,54 @@ const DefaultView = props => {
             </div>
 
             {/*MOVIE FILTERS*/}
-            <div class="h-screen col-span-2 row-span-10 bg-blue-300 rounded-xl">
-                <p>test</p>
+            <div class="h-screen col-span-2 row-span-10 bg-blue-300 rounded-xl p-8 ">
+                <p class="text-2xl text-center font-semibold pb-16">Movie Filters</p>
+
+                {/*TITLE INPUT*/}
+                <form class="flex justify-between items-center pb-8">
+                    <input id="radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                    <label for="radio-1" class="text-2xl font-semibold">Title</label>
+                    <input class="searchBar-area"></input>
+                </form>
+
+                {/*GENRE INPUT*/}
+                <form class="flex justify-between items-center pb-8">
+                    <input id="radio-2" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                    <label for="radio-2" class="text-2xl font-semibold">Genre</label>
+                    <input class="searchBar-area"></input>
+                </form>
+
+                {/*YEAR INPUTS*/}
+                <form class="flex-co justify-between items-center pb-8">
+                    
+                    <div class="flex justify-between items-center">
+                        <input id="radio-3" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                        <label for="radio-3" class="text-2xl font-semibold">Year</label>
+                        <input class="searchBar-area invisible"></input>
+                    </div>
+
+                    <div class="flex justify-end pt-4">
+                        <label for="year-less" class="text-xl font-semibold pr-2">Less</label>
+                        <input id ="year-less" class="searchBar-area"></input>
+                    </div>
+
+                    <div class="flex justify-end pt-4">
+                        <label for="year-greater" class="text-xl font-semibold pr-2">Greater</label>
+                        <input id ="year-greater" class="searchBar-area"></input>
+                    </div>
+                    
+                </form>
+                
             </div>
 
             {/*LIST/MATCHES*/}
-            <div class="h-screen col-span-4 row-span-10 bg-blue-300 rounded-xl">
-                <p>test</p>
+            <div class="h-screen col-span-4 row-span-10 bg-blue-300 rounded-xl p-8">
+                <p class="text-2xl text-center font-semibold pb-16">List/Matches</p>
             </div>
 
             {/*FAVORITES*/}
-            <div class="h-screen col-start-7 col-span-2 row-span-10 bg-blue-300 rounded-xl">
-                <p>test</p>
+            <div class="h-screen col-start-7 col-span-2 row-span-10 bg-blue-300 rounded-xl p-8">
+                <p class="text-2xl text-center font-semibold pb-16">Favorites</p>
             </div>
             
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = props => {
     return (
-        <header class="flex justify-between items-center bg-blue-300 p-7 rounded-b-xl"></header>
+        <header class="flex justify-between items-center bg-blue-300 p-8 rounded-b-xl"></header>
     )
 }
 
