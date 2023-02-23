f02258b79d62e33f80ee027dcfc55d445b0a888b
Shargeel Hayat <59932594+ShargHayat518@users.noreply.github.com>
2023-02-23 15:08:36 -0700

    improvements to default_view

--- src/components/DefaultView.js
+++ src/components/DefaultView.js
@@ -3,22 +3,28 @@ import Header from './Header'
 
 const DefaultView = props => {
     return (
-        <div class="grid grid-cols-8 grid-rows-10 gap-1">
+        <div class="grid grid-cols-8 grid-rows-10 gap-10">
+
             {/*LOGO*/}
             <div class="col-span-8">
                 <Header></Header>
             </div>
 
-
-
-
             {/*MOVIE FILTERS*/}
-            <div class="col-span-2 row-span-10 bg-blue-300">
+            <div class="h-screen col-span-2 row-span-10 bg-blue-300 rounded-xl">
                 <p>test</p>
             </div>
+
             {/*LIST/MATCHES*/}
+            <div class="h-screen col-span-4 row-span-10 bg-blue-300 rounded-xl">
+                <p>test</p>
+            </div>
 
             {/*FAVORITES*/}
+            <div class="h-screen col-start-7 col-span-2 row-span-10 bg-blue-300 rounded-xl">
+                <p>test</p>
+            </div>
+            
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = props => {
     return (
-        <header class="flex justify-between items-center bg-blue-300 p-4"></header>
+        <header class="flex justify-between items-center bg-blue-300 p-7 rounded-b-xl"></header>
     )
 }
 
