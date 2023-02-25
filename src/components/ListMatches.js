4826708eb46d2f15d000c6dc7972c4752fd9a994
myusername <jkelln24@gmail.com>
2023-02-24 19:59:47 -0700

    dynamic list created

--- src/components/ListMatches.js
+++ src/components/ListMatches.js
@@ -2,11 +2,25 @@ import React from 'react';
 
 const ListMatches = props => {
 
-    let movieExample = props.movies[0];
-
-    const imgURL = `https://image.tmdb.org/t/p/w92${movieExample.poster}`;
-
+    let movieExample = props.movies[1];
     console.log(movieExample);
+
+    const movieItem = (movie) => {
+
+        const imgURL = `https://image.tmdb.org/t/p/w92${movie.poster}`;
+
+        return (
+            <div class="grid grid-cols-10 pt-8 items-center">
+                <img class="col-start-1 col-span-1" width="92" height="92" src={imgURL} alt={movie.title}></img>
+                <p class="col-start-2 col-span-3 r">{movie.title}</p>
+                <p class="col-start-5 col-span-1">{movie.release_date}</p>
+                <p class="col-start-6 col-span-1">{movie.ratings.average}</p>
+                <p class="col-start-7 col-span-1">{movie.ratings.average}</p>
+                <button class="col-start-9 col-span-1">❤</button>
+                <button class="col-start-10 col-span-1">View</button>
+            </div>
+        );
+    }
 
     return (
         <div class="h-screen col-start-3 col-span-4 row-span-10 bg-blue-300 rounded-xl p-8 ">
@@ -19,23 +33,17 @@ const ListMatches = props => {
                 <p class="col-start-7 col-span-1">Popularity</p>
             </div>
 
-            <div class="grid grid-cols-10 pt-8 items-center">
-                {/* <img class="col-start-1 col-span-1" width="92" height="92" src="https://icons.iconarchive.com/icons/iconsmind/outline/256/Film-icon.png" alt="img"></img>
+            {props.movies.map((m) => movieItem(m))}
+
+            {/*<div class="grid grid-cols-10 pt-8 items-center">
+                <img class="col-start-1 col-span-1" width="92" height="92" src="https://icons.iconarchive.com/icons/iconsmind/outline/256/Film-icon.png" alt="img"></img>
                 <p class="col-start-2 col-span-3 r">Title</p>
                 <p class="col-start-5 col-span-1">Year</p>
                 <p class="col-start-6 col-span-1">Rating</p>
                 <p class="col-start-7 col-span-1">Popularity</p>
                 <p class="col-start-9 col-span-1">Heart</p>
-                <p class="col-start-10 col-span-1">View</p>*/}
-
-                <img class="col-start-1 col-span-1" width="92" height="92" src={imgURL} alt={movieExample.title}></img>
-                <p class="col-start-2 col-span-3 r">{movieExample.title}</p>
-                <p class="col-start-5 col-span-1">{Date.parse(movieExample.release_date)[0]}</p>
-                <p class="col-start-6 col-span-1">{movieExample.ratings.average}</p>
-                <p class="col-start-7 col-span-1">{movieExample.ratings.average}</p>
-                <p class="col-start-9 col-span-1">Heart</p>
                 <p class="col-start-10 col-span-1">View</p>
-            </div>
+    </div>*/}
 
         </div>
     )
