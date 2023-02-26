264c6f4ef7dcbc85683ae225f992f37351fe746d
Shargeel Hayat <59932594+ShargHayat518@users.noreply.github.com>
2023-02-25 17:38:44 -0700

    more improvements to filter functionality

--- src/components/MovieFilters.js
+++ src/components/MovieFilters.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 class MovieFilters extends React.Component{
+
     constructor(){
         super();
         this.state = {
-            selectedRadio: null
+            selectedRadio: null,
+            titleDisabled: null,
+            genreDisabled: null,
+            yearDisabled: null,
+            ratingDisabled: null,
         }
 
         this.radioButtonSelected = this.radioButtonSelected.bind(this);
@@ -13,17 +18,60 @@ class MovieFilters extends React.Component{
 
     radioButtonSelected(value){
         this.setState({
-            selectedRadio: value.target.value
+            selectedRadio: value.target.value,
         });
+        //Handles disabling other inputs
+        switch(value.target.value){
+            case 'title':
+                this.setState({
+                    genreDisabled: true,
+                    yearDisabled: true,
+                    ratingDisabled: true
+                })
+            break
+            case 'genre':
+                this.setState({
+                    titleDisabled: true,
+                    yearDisabled: true,
+                    ratingDisabled: true
+                })
+            break
+            case 'year':
+                this.setState({
+                    titleDisabled: true,
+                    genreDisabled: true,
+                    ratingDisabled: true
+                })
+            break
+            case 'rating':
+                this.setState({
+                    titleDisabled: true,
+                    genreDisabled: true,
+                    yearDisabled: true
+                })
+            break
+
+            default:
+                return null
+        }
 
         console.log(`VALUE IS: ${value.target.value}`)
     }
 
-    //Resets all radio buttons
+    //Resets all radio buttons & text inputs
     reset(){
         this.setState({
-            selectedRadio: null
+            selectedRadio: null,
+            titleDisabled: null,
+            genreDisabled: null,
+            yearDisabled: null,
+            ratingDisabled: null,
         });
+
+        this.clearTitle.reset();
+        this.clearGenre.reset();
+        this.clearYear.reset();
+        this.clearRating.reset();
     }
 
     render(){
@@ -32,57 +80,57 @@ class MovieFilters extends React.Component{
                 <p class="text-2xl text-center font-semibold pb-16">Movie Filters</p>
 
                 {/*TITLE INPUT*/}
-                <form class="flex justify-between items-center pb-8">
-                    <input onChange={this.radioButtonSelected} checked={this.state.selectedRadio === "title"} id="radio-1" type="radio" value="title" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                <form ref={(el) => {this.clearTitle = el;}} class="flex justify-between items-center pb-8">
+                    <input disabled={(this.state.titleDisabled) ? "disabled":""} value="title" onChange={this.radioButtonSelected} checked={this.state.selectedRadio === "title"} id="radio-1" type="radio" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
                     <label for="radio-1" class="text-2xl font-semibold">Title</label>
-                    <input class="searchBar-area"></input>
+                    <input disabled={(this.state.titleDisabled) ? "disabled":""} id="title" class="searchBar-area"></input>
                 </form>
 
                 {/*GENRE INPUT*/}
-                <form class="flex justify-between items-center pb-8">
-                    <input onChange={this.radioButtonSelected} checked={this.state.selectedRadio === "genre"} id="radio-2" type="radio" value="genre" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                <form ref={(el) => {this.clearGenre = el;}} class="flex justify-between items-center pb-8">
+                    <input disabled={(this.state.genreDisabled) ? "disabled":""} value="genre" onChange={this.radioButtonSelected} checked={this.state.selectedRadio === "genre"} id="radio-2" type="radio" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
                     <label for="radio-2" class="text-2xl font-semibold">Genre</label>
-                    <input class="searchBar-area"></input>
+                    <input disabled={(this.state.genreDisabled) ? "disabled":""} class="searchBar-area"></input>
                 </form>
 
                 {/*YEAR INPUTS*/}
-                <form class="flex-co justify-between items-center pb-8">
+                <form ref={(el) => {this.clearYear = el;}} class="flex-co justify-between items-center pb-8">
 
                     <div class="flex justify-between items-center">
-                        <input onChange={this.radioButtonSelected} checked={this.state.selectedRadio === "year"} id="radio-3" type="radio" value="year" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                        <input disabled={(this.state.yearDisabled) ? "disabled":""} onChange={this.radioButtonSelected} checked={this.state.selectedRadio === "year"} id="radio-3" type="radio" value="year" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
                         <label for="radio-3" class="text-2xl font-semibold">Year</label>
-                        <input class="searchBar-area invisible"></input>
+                        <input disabled={(this.state.yearDisabled) ? "disabled":""} class="searchBar-area invisible"></input>
                     </div>
 
                     <div class="flex justify-end pt-4">
                         <label for="year-less" class="text-xl font-semibold pr-2">Less</label>
-                        <input id="year-less" class="searchBar-area"></input>
+                        <input disabled={(this.state.yearDisabled) ? "disabled":""} id="year-less" class="searchBar-area"></input>
                     </div>
 
                     <div class="flex justify-end pt-4">
                         <label for="year-greater" class="text-xl font-semibold pr-2">Greater</label>
-                        <input id="year-greater" class="searchBar-area"></input>
+                        <input disabled={(this.state.yearDisabled) ? "disabled":""} id="year-greater" class="searchBar-area"></input>
                     </div>
 
                 </form>
 
                 {/*RATING INPUTS*/}
-                <form class="flex-co justify-between items-center pb-8">
+                <form ref={(el) => {this.clearRating = el;}} class="flex-co justify-between items-center pb-8">
 
                     <div class="flex justify-between items-center">
-                        <input onChange={this.radioButtonSelected} checked={this.state.selectedRadio === "rating"} id="radio-3" type="radio" value="rating" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                        <input disabled={(this.state.ratingDisabled) ? "disabled":""} onChange={this.radioButtonSelected} checked={this.state.selectedRadio === "rating"} id="radio-3" type="radio" value="rating" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
                         <label for="radio-3" class="text-2xl font-semibold">Rating</label>
-                        <input class="searchBar-area invisible"></input>
+                        <input disabled={(this.state.ratingDisabled) ? "disabled":""} class="searchBar-area invisible"></input>
                     </div>
 
                     <div class="flex justify-end pt-4">
                         <label for="year-less" class="text-xl font-semibold pr-2">Less</label>
-                        <input id="year-less" class="searchBar-area"></input>
+                        <input disabled={(this.state.ratingDisabled) ? "disabled":""} id="year-less" class="searchBar-area"></input>
                     </div>
 
                     <div class="flex justify-end pt-4">
                         <label for="year-greater" class="text-xl font-semibold pr-2">Greater</label>
-                        <input id="year-greater" class="searchBar-area"></input>
+                        <input disabled={(this.state.ratingDisabled) ? "disabled":""} id="year-greater" class="searchBar-area"></input>
                     </div>
                 </form>
 
