3327da866cf9ebd244fa3b5d0f5af65bdba81f36
myusername <jkelln24@gmail.com>
2023-02-27 23:57:49 -0700

    added rating stars to details

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import ReactStars from 'react-rating-stars-component';
 
 const MovieDetails = props => {
 
@@ -13,6 +14,16 @@ const MovieDetails = props => {
     const imdbURL = `https://www.imdb.com/title/${state.imdb_id}`;
 
     const revenue = state.revenue;
+
+    //Retrieved from Stackoverflow at: https://stackoverflow.com/questions/6137986/javascript-roundoff-number-to-nearest-0-5
+    const round = number => {
+        var rating = (number * 2).toFixed() / 2;
+        return rating;
+    }
+
+    const changeEdit = () => {
+
+    }
 
     return (
         <div class="grid gap-10">
@@ -29,9 +40,6 @@ const MovieDetails = props => {
                 {/*MIDDLE*/}
                 <div class="col-start-2 col-span-2 border-solid border-black border-2 p-16">
                     <div class="pt-20 pb-20 font-semibold text-2xl">
-                        {/*<p>Release Date, runtime, tagline, revenue</p>
-                        <p>IMDB Link, TMDB Link</p>
-    <p>Genres</p>*/}
 
                         <p>{state.release_date}, {state.runtime}, {state.tagline}, ${revenue.toLocaleString()}</p>
                         <p>{tmdbURL}, {imdbURL}</p>
@@ -42,6 +50,33 @@ const MovieDetails = props => {
                     <div class="col-start-4 border-solid border-black border-2 p-16">
                         <h2 class="font-semibold text-3xl text-center">Ratings</h2>
                         <p>{state.ratings.popularity}, {state.ratings.average}, {state.ratings.count}</p>
+
+                        {/*Retrieved from Star Rating Component for your project: React Stars. At: https://morioh.com/p/98335027daaa */}
+                        <ReactStars
+                            className={"fixedRating"}
+                            count={10}
+                            value={round(state.ratings.average)}
+                            size={24}
+                            edit={false}
+                            isHalf={true}
+                            emptyIcon={<i className="far fa-star"></i>}
+                            halfIcon={<i className="fa fa-star-half-alt"></i>}
+                            fullIcon={<i className="fa fa-star"></i>}
+                            activeColor="#ffd700"
+                        />
+                        <h2 className='userRating'>Leave a rating</h2>
+                        <ReactStars
+                            className={"userRating"}
+                            count={10}
+                            size={24}
+                            onChange={changeEdit}
+                            edit={true}
+                            isHalf={true}
+                            emptyIcon={<i className="far fa-star"></i>}
+                            halfIcon={<i className="fa fa-star-half-alt"></i>}
+                            fullIcon={<i className="fa fa-star"></i>}
+                            activeColor="#ffd700"
+                        />
                     </div>
                 </div>
 
