c6fd6b05f4f7f691658164dc0c49fb6772ecf0e0
myusername <jkelln24@gmail.com>
2023-02-28 22:25:24 -0700

    implemented FavItem.js

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -24,18 +24,6 @@ const Favorites = props => {
         return (
             <FavoritesItem movie={state} />
         )
-        {/*<div class="h-screen col-start-7 col-span-2 row-span-10 bg-blue-300 rounded-xl p-8 ">
-                <p class="text-2xl text-center font-semibold pb-16">Favorites</p>
-
-
-                <div class="grid grid-cols-2 items-center">
-                    <Link to='/list'>
-                        <img class="col-start-1 col-span-1" width="92" height="92" src={imgURL} alt={state.title}></img>
-                        <p class="col-start-2 col-span-3 r">{state.title}</p>
-                    </Link>
-                </div>
-        </div >*/}
-
     }
 }
 
