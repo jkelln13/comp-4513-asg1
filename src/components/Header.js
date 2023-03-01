6594c48a0b6fd3d0ceb65172d15507010fa5441f
myusername <jkelln24@gmail.com>
2023-02-28 23:06:58 -0700

    added about modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,6 @@ import About from './About'
 
 const Header = props => {
 
-    // const navigate = useNavigate()
-
-    // const goToHomeview = () =>{
-    //     navigate(<HomeView></HomeView>)
-    // }
     const [modal, setModal] = useState(false);
 
     const toggleModal = () => {
@@ -19,7 +14,6 @@ const Header = props => {
     return (
         <header class="flex justify-between items-center bg-blue-300 rounded-b-xl">
             <img class="mx-2 my-2" width="60" height="60" src="https://icons.iconarchive.com/icons/iconsmind/outline/256/Film-icon.png" alt="logo"></img>
-            {/*(<button class="mx-5 my-5 bg-slate-600 hover:bg-indigo-700 text-white text-base py-3 px-10 rounded" onClick={toggleModal}>About</button>*/}
             <About />
         </header>
     )
