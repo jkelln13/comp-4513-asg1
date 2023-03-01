7d1ccb5fcbd02c61cdc0baaf82335754b11b3241
myusername <jkelln24@gmail.com>
2023-02-28 23:11:25 -0700

    added link home to logo in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import About from './About'
+import { Link } from 'react-router-dom';
 
 const Header = props => {
 
     return (
         <header class="flex justify-between items-center bg-blue-300 rounded-b-xl">
-            <img class="mx-2 my-2" width="60" height="60" src="https://icons.iconarchive.com/icons/iconsmind/outline/256/Film-icon.png" alt="logo"></img>
+            <Link to='/'>
+                <img class="mx-2 my-2" width="60" height="60" src="https://icons.iconarchive.com/icons/iconsmind/outline/256/Film-icon.png" alt="logo"></img>
+            </Link>
             <About />
         </header>
     )
